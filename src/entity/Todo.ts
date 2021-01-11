66ee31eb99b6d87e836ac9fd3c169c553a92aae4
sairaj <sairaj2119@gmail.com>
2021-01-11 23:16:53 +0530

    add basic create and list of todos

--- src/entity/Todo.ts
+++ src/entity/Todo.ts
@@ -1,10 +1,23 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+} from 'typeorm';
 
 @Entity('todos')
-export class Todo {
+export class Todo extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   text: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,48 @@
 import 'reflect-metadata';
+import dotenv from 'dotenv';
+dotenv.config();
+import { createConnection } from 'typeorm';
 import express, { Request, Response } from 'express';
+import { Todo } from './entity/Todo';
 
 (() => {
   const app = express();
+
+  app.use(express.json());
 
   app.get('/', (_: Request, res: Response) => {
     console.log('NODE_ENV is => ', process.env.NODE_ENV);
     console.log('PORT is => ', process.env.PORT);
     console.log('TEST env var is => ', process.env.TEST);
+    console.log('YOO is => ', process.env.YOO);
     res.send('yes this is woking');
   });
 
+  app.get('/todos', async (_: Request, res: Response) => {
+    const todos = await Todo.find();
+    return res.json(todos);
+  });
+
+  app.post('/todos', async (req: Request, res: Response) => {
+    const { text } = req.body;
+    if (text.trim() === '')
+      return res.status(400).json({ error: 'todo canont be empty' });
+    const todo = Todo.create();
+
+    todo.text = text;
+    await todo.save();
+    return res.json(todo);
+  });
+
   const PORT = process.env.PORT || 5000;
+  createConnection()
+    .then(() => {
+      console.log('connected to databse');
+    })
+    .catch((err) => {
+      console.log('error in connecting to database');
+      console.log(err);
+    });
   app.listen(PORT, () => {
     console.log('server is runing at port 5000');
   });
