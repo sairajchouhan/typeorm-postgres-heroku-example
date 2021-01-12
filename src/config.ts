af958e3ab68128d2d36db552675a8ae1d898c118
sairaj <sairaj2119@gmail.com>
2021-01-12 11:46:36 +0530

    trying to host take-3

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,17 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-
 import { Todo } from './entity/Todo';
 
 const prodConnection: PostgresConnectionOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url: `${process.env.DATABASE_URL}?sslmode=require`,
   synchronize: false,
   logging: false,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  // extra: {
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
+  // },
+  ssl: true,
   entities: [Todo],
 };
 
